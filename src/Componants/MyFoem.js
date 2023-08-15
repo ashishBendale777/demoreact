6955fed01d1b3c087bfbf04efc847cb827ddcf59
ashishBendale777 <36222108+ashishBendale777@users.noreply.github.com>
2023-08-15 20:11:24 +0530

    form data

--- src/Componants/MyFoem.js
+++ src/Componants/MyFoem.js
@@ -1,9 +1,13 @@
 import React, { useState } from 'react'
+import { Form } from 'react-bootstrap'
 
 const MyFoem = () => {
     const [counter, setcounter] = useState(0)
+    const [firstName, setFirstName] = useState("")
+    const [lastName, setLastName] = useState("")
+    const [gender, setGender] = useState("")
 
-    var names=["Ashish","Pawn","Dhiraj"]
+    var names = ["Ashish", "Pawn", "Dhiraj"]
 
     function incre() {
         setcounter(counter + 1)
@@ -14,9 +18,26 @@ const MyFoem = () => {
 
     return (
         <div>
-            <button onClick={() => decre()}>Prev</button>
+            {/* <button onClick={() => decre()}>Prev</button>
             <h3>{names[counter]}</h3>
-            <button onClick={() => incre()}>Next</button>
+            <button onClick={() => incre()}>Next</button> */}
+
+            <Form>
+                <Form.Group>
+                    <Form.Label>First Name</Form.Label>
+                    <Form.Control type='Text' placeholder='Enter Name' onChange={(e) => setFirstName(e.target.value)} />
+                </Form.Group>
+                <Form.Group>
+                    <Form.Label>Last Name</Form.Label>
+                    <Form.Control type='Text' placeholder='Enter Last Name' onChange={(e) => setLastName(e.target.value)} />
+                </Form.Group>
+                <Form.Group>
+                    <Form.Check type="radio" label="Male" name="gender" value="Male" onChange={(e) => setGender(e.target.value)} />
+                    <Form.Check type="radio" label="Female" name="gender" value="FeMale" onChange={(e) => setGender(e.target.value)} />
+                </Form.Group>
+            </Form>
+            <button>Submit</button>
+            <p>{firstName + " " + lastName + " " + gender}</p>
         </div>
     )
 }
