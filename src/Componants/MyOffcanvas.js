09c34f9741d706120f8f992c403a55e4cacc1065
ashishBendale777 <36222108+ashishBendale777@users.noreply.github.com>
2023-10-30 11:58:53 +0530

    d0he

--- src/Componants/MyOffcanvas.js
+++ src/Componants/MyOffcanvas.js
@@ -2,11 +2,11 @@ import React from 'react'
 import { useState } from 'react'
 import { Container, Nav, Navbar, Offcanvas } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
-
+import { MdMapsHomeWork } from 'react-icons/md'
 const MyOffcanvas = () => {
 
     const [showCanvas, setshowCanvas] = useState(false)
-
+    const [currentLoc, setCurrentLoc] = useState("")
 
 
     return (
@@ -23,18 +23,32 @@ const MyOffcanvas = () => {
                 </Offcanvas.Header>
                 <Offcanvas.Body>
                     <Nav className="d-flex flex-column">
-                        <Nav.Link onClick={()=>setshowCanvas(false)}>
+                        <Nav.Link onClick={() => {
+                            setshowCanvas(false)
+                            setCurrentLoc(window.location.href.split('/')[3])
+                        }}>
+                            <MdMapsHomeWork size={26} className='mr-3' color={currentLoc === '' ? 'red' : "blue"} />
                             <Link to='/'>Home</Link>
                         </Nav.Link>
-                        <Nav.Link onClick={()=>setshowCanvas(false)}>
+                        <Nav.Link onClick={() => {
+                            setCurrentLoc(window.location.href.split('/')[3])
+                            setshowCanvas(false)
+                        }}>
+                            <MdMapsHomeWork size={26} className='mr-3' color={currentLoc === 'about' ? 'red' : "blue"} />
                             <Link to='/about'>About</Link>
                         </Nav.Link>
-                        <Nav.Link onClick={()=>setshowCanvas(false)}>
+                        <Nav.Link onClick={() => {
+                            setCurrentLoc(window.location.href.split('/')[3])
+                            setshowCanvas(false)
+                        }}>
+                            <MdMapsHomeWork size={26} className='mr-3' color={currentLoc === 'contact' ? 'red' : "blue"} />
                             <Link to='/contact'>Contact Us</Link>
                         </Nav.Link>
                     </Nav>
                 </Offcanvas.Body>
             </Offcanvas>
+
+            {console.log("LOCARION", window.location.href.split('/')[3])}
         </div>
     )
 }
